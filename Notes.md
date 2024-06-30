272cec5d50322295e6f4bda4e09a4e7b7c7f0d69
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2024-06-30 11:25:20 +0200

    add some files for the elixir learning.

--- Notes.md
+++ Notes.md
@@ -1,16 +1,16 @@
-# apprentissage du language Exilir.
+# apprentissage du langage Elixir.
 
 
 - [x] Installer les outils
 - [x] Faire les exos dans learnXinYminutes
-- [ ]  lire le ebook elixir WIP
+- [ ]  lire le ebook Elixir WIP
 
 
-## Comment instancier un module dans le IEX ??
+## Comment instancié un module dans le IEX ??
 ```bash
 iex Recursions.exs
 ```
 
-et ensuite faire Examples.instancePerson pour voir un **Struct**.
+Et ensuite faire Examples.instancePerson pour voir un **Struct**.
 
 01/05/2024: fin des TPs de learn X in Y minutes Elixir
---
+++ simple_queue/lib/simple_queue.ex
@@ -0,0 +1,40 @@
+defmodule SimpleQueue do
+  use GenServer
+
+  ### GenServer API
+
+  @doc """
+  GenServer.init/1 callback
+  """
+  def init(state), do: {:ok, state}
+
+  @doc """
+  GenServer.handle_call/3 callback
+  """
+  def handle_call(:dequeue, _from, [value | state]) do
+    {:reply, value, state}
+  end
+
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
+
+  def handle_call(:queue, _from, state), do: {:reply, state, state}
+
+  ### Client API / Helper functions
+
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def queue, do: GenServer.call(__MODULE__, :queue)
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+
+  def child_spec(opts) do
+    %{
+      id: SimpleQueue,
+      start: {__MODULE__, :start_link, [opts]},
+      shutdown: 5_000,
+      restart: :permanent,
+      type: :worker
+    }
+  end
+end
